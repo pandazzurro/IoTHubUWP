6b343d86de6bab84df5a6c850d0e9aea8e1a7cfa
Andrea Tosato <andrea.tosato@hotmail.it>
2017-04-07 22:24:17 +0200

    Update and commit lib

--- AzureIoTHub.Device.UWP/MainPage.xaml.cs
+++ AzureIoTHub.Device.UWP/MainPage.xaml.cs
@@ -1,17 +1,35 @@
-﻿using Windows.ApplicationModel.Background;
+﻿using Microsoft.Azure.Devices.Client;
 using System;
-using Microsoft.Azure.Devices.Client;
+using System.Collections.Generic;
 using System.IO;
-using Windows.Storage;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
 
-namespace AzureIoTHub.Device
+// Il modello di elemento Pagina vuota è documentato all'indirizzo https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x410
+
+namespace AzureIoTHub.Device.UWP
 {
-    public sealed class StartupTask : IBackgroundTask
+    /// <summary>
+    /// Pagina vuota che può essere usata autonomamente oppure per l'esplorazione all'interno di un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
     {
-        public async void Run(IBackgroundTaskInstance taskInstance)
+        public MainPage()
         {
-            var deferral = taskInstance.GetDeferral();
+            this.InitializeComponent();
+
+
             try
             {
                 Task.Run(async () =>
